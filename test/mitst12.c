f69e79678d215b082b0caf6ab142e5e2570e11c0
whaley <whaley>
2003-09-29 18:43:06 +0000

    imul test added

--- test/mitst12.c
+++ test/mitst12.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /*
- * This tests whether iFKO can do integer division
+ * This tests whether iFKO can do integer multiplication
  */
 main()
 {
-   int top=(-8), bot=3;
-   int iexp=top/bot, iret;
-   int div_tst(int top, int bot);
-   iret = div_tst(top, bot);
-   if (iret == iexp) fprintf(stdout, "11: IDIV TEST PASSED\n");
+   int m0=(-7), m1=3;
+   int iexp=m0*m1, iret;
+   int mul_tst(int top, int bot);
+   iret = mul_tst(m0, m1);
+   if (iret == iexp) fprintf(stdout, "12: IMUL TEST PASSED\n");
    else
-      fprintf(stderr, "11: IDIV TEST FAILED: iexp=%d, iret=%d\n", iexp, iret);
+      fprintf(stderr, "12: IMUL TEST FAILED: iexp=%d, iret=%d\n", iexp, iret);
    exit(iret-iexp);
 }
