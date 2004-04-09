a84896f025e30af41c2e2877eb30540ea6a444e2
whaley <whaley>
2004-04-09 15:00:39 +0000

    *** empty log message ***

---
+++ test/mitst14.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*
+ * This tests whether iFKO can multiply two ints, using a loop
+ */
+main()
+{
+   int multiply(int, int);
+   const int ip0 = 3, ip1 = 7;
+   int i, iret, iexp=ip0*ip1;
+   iret = multiply(ip0, ip1);
+   if (iret == iexp) fprintf(stdout, "14i: IMULTIPLY LOOP PASSED\n");
+   else fprintf(stderr, "7i: IMULTIPLY LOOP FAILED: got=%d, expected=%d\n",
+                iret, iexp);
+   exit(iret-iexp);
+}
