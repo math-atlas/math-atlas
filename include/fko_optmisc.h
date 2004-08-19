8b8ae4fe243f31a0e4d7363d5720742294e00e8f
whaley <whaley>
2004-08-19 18:21:54 +0000

    new files

---
+++ include/fko_optmisc.h
@@ -0,0 +1,4 @@
+#ifndef FKO_OPTMISC_H
+   #define FKO_OPTMISC_H
+   int DoStoreNT(BLIST *scope);
+#endif
---
+++ src/optmisc.c
@@ -0,0 +1,45 @@
+#include "fko.h"
+
+int DoStoreNT(BLIST *scope)
+/*
+ * Changes all stores of selected arrays to non-temporal (cache-through) form
+ */
+{
+   BLIST *bl;
+   int i;
+   int nchanges=0;
+   short k, st;
+   INSTQ *ip;
+   enum inst store;
+   extern int NWNT;
+   extern char **ARRWNT;
+   BLIST *killme;
+
+   if (!scope)
+      killme = scope = GetGlobalScope();
+   for (i=0; i < NWNT; i++)
+   {
+      st = FindVarFromName(ARRWNT[i]);
+      k = st + TNREG - 1;
+      assert(st);
+      for (bl=scope; bl; bl = bl->next)
+      {
+         for (ip=bl->blk->ainst1; ip; ip = ip->next)
+         {
+            store = 0;
+            if (ip->inst[0] == VFST)
+               store = VFSTNT;
+            else if (ip->inst[0] == VDST)
+               store = VDSTNT;
+            if (store && STpts2[ip->inst[1]-1] == st)
+            {
+               ip->inst[0] = store;
+               nchanges++;
+            }
+         }
+      }
+   }
+   if (killme)
+      KillBlockList(killme);
+   return(nchanges);
+}
