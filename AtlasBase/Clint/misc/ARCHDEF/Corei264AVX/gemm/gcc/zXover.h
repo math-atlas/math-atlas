20792b42a2d9e92054624b11ddefb988f5840b7d
R. Clint Whaley <whaley@cs.utsa.edu>
2011-04-06 18:44:32 -0500

    fixed Corei2AVX arch defs

--- AtlasBase/Clint/misc/ARCHDEF/Corei264AVX/gemm/gcc/zXover.h
+++ AtlasBase/Clint/misc/ARCHDEF/Corei264AVX/gemm/gcc/zXover.h
@@ -1,27 +1,27 @@
 #ifndef ZXOVER_H
 #define ZXOVER_H
 
-#define ATL_3NB 84
-#define NN_MNK_M 2800
-#define NN_MNK_N 6300
-#define NN_MNK_MN 7840
-#define NN_MNK_K 6300
-#define NN_MNK_GE 3375
-#define NT_MNK_M 6300
-#define NT_MNK_N 6300
-#define NT_MNK_MN 7840
-#define NT_MNK_K 6300
-#define NT_MNK_GE 3375
-#define TN_MNK_M 6300
-#define TN_MNK_N 6300
-#define TN_MNK_MN 7840
-#define TN_MNK_K 6300
-#define TN_MNK_GE 3375
-#define TT_MNK_M 2800
-#define TT_MNK_N 6300
-#define TT_MNK_MN 7840
-#define TT_MNK_K 6300
-#define TT_MNK_GE 3375
-#define C2R_K 9223372036854775807
+#define ATL_3NB 144
+#define NN_MNK_M 69312
+#define NN_MNK_N 69312
+#define NN_MNK_MN 23040
+#define NN_MNK_K 69312
+#define NN_MNK_GE 54872
+#define NT_MNK_M 69312
+#define NT_MNK_N 69312
+#define NT_MNK_MN 23040
+#define NT_MNK_K 69312
+#define NT_MNK_GE 54872
+#define TN_MNK_M 69312
+#define TN_MNK_N 10800
+#define TN_MNK_MN 23040
+#define TN_MNK_K 27648
+#define TN_MNK_GE 27000
+#define TT_MNK_M 69312
+#define TT_MNK_N 43200
+#define TT_MNK_MN 23040
+#define TT_MNK_K 27648
+#define TT_MNK_GE 54872
+#define C2R_K 246
 
 #endif
