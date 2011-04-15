54761b01e31daa0e9b006147a9cb15ff8c1aa890
R. Clint Whaley <whaley@cs.utsa.edu>
2011-04-15 10:00:26 -0500

    simple tester improvements

--- AtlasTest/scope.sh
+++ AtlasTest/scope.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 # USAGE: ./scope.sh <arch>
 echo ================ CBLAS ==================
-fgrep -e fault -e FAULT -e error -e ERROR -e fail -e FAIL res/C_BLAS* | \
+fgrep -e fault -e FAULT -e error -e ERROR -e fail -e FAIL -e Abort -e ABORT \
+      -e abort res/C_BLAS* | \
       fgrep -v PASSED
 echo ================ F77BLAS ==================
-fgrep -e fault -e FAULT -e error -e ERROR -e fail -e FAIL res/F77_BLAS* | \
-      fgrep -v PASSED
+fgrep -e fault -e FAULT -e error -e ERROR -e fail -e FAIL -e Abort -e ABORT \
+      -e abort res/F77_BLAS* | fgrep -v  PASSED
 echo ================== BIN ====================
-fgrep -e fault -e FAULT -e error -e ERROR -e fail -e FAIL res/BIN_SUMM
+fgrep -e fault -e FAULT -e error -e ERROR -e fail -e FAIL -e Abort -e ABORT \
+      -e abort res/BIN_SUMM
 echo ================= PTBIN ===================
-fgrep -e fault -e FAULT -e error -e ERROR -e fail -e FAIL res/BIN_PTSUMM
+fgrep -e fault -e FAULT -e error -e ERROR -e fail -e FAIL -e Abort -e ABORT \
+      -e abort res/BIN_PTSUMM
 echo ================= DONE  ===================
 
