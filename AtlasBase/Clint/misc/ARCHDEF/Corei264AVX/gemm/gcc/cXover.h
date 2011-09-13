fd074b5583ee5710e60b2d3dc12399a0ecdd63b9
R. Clint Whaley <whaley@cs.utsa.edu>
2011-09-13 09:24:09 -0500

    arch defs use new AVX kernels

--- AtlasBase/Clint/misc/ARCHDEF/Corei264AVX/gemm/gcc/cXover.h
+++ AtlasBase/Clint/misc/ARCHDEF/Corei264AVX/gemm/gcc/cXover.h
@@ -1,27 +1,27 @@
 #ifndef CXOVER_H
 #define CXOVER_H
 
-#define ATL_3NB 240
-#define NN_MNK_M 72000
-#define NN_MNK_N 176720
-#define NN_MNK_MN 243200
-#define NN_MNK_K 224720
-#define NN_MNK_GE 148877
-#define NT_MNK_M 224720
-#define NT_MNK_N 115520
-#define NT_MNK_MN 243200
-#define NT_MNK_K 115520
-#define NT_MNK_GE 148877
-#define TN_MNK_M 224720
-#define TN_MNK_N 224720
-#define TN_MNK_MN 243200
-#define TN_MNK_K 176720
-#define TN_MNK_GE 148877
-#define TT_MNK_M 72000
-#define TT_MNK_N 176720
-#define TT_MNK_MN 192000
-#define TT_MNK_K 297680
-#define TT_MNK_GE 54872
-#define C2R_K 76
+#define ATL_3NB 216
+#define NN_MNK_M 7200
+#define NN_MNK_N 7200
+#define NN_MNK_MN 51840
+#define NN_MNK_K 64800
+#define NN_MNK_GE 13824
+#define NT_MNK_M 7200
+#define NT_MNK_N 7200
+#define NT_MNK_MN 51840
+#define NT_MNK_K 64800
+#define NT_MNK_GE 13824
+#define TN_MNK_M 7200
+#define TN_MNK_N 7200
+#define TN_MNK_MN 51840
+#define TN_MNK_K 103968
+#define TN_MNK_GE 13824
+#define TT_MNK_M 7200
+#define TT_MNK_N 7200
+#define TT_MNK_MN 51840
+#define TT_MNK_K 103968
+#define TT_MNK_GE 27000
+#define C2R_K 153
 
 #endif
