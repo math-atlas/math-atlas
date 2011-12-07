eb43a9943a7f645be2c45e9da14e767dafdb1662
R. Clint Whaley <whaley@cs.utsa.edu>
2011-12-07 16:41:44 -0600

    new Corei264AVX arch defs

--- AtlasBase/Clint/misc/ARCHDEF/Corei264AVX/gemm/gcc/dXover.h
+++ AtlasBase/Clint/misc/ARCHDEF/Corei264AVX/gemm/gcc/dXover.h
@@ -1,26 +1,26 @@
 #ifndef DXOVER_H
 #define DXOVER_H
 
-#define ATL_3NB 90
-#define NN_MNK_M 17280
-#define NN_MNK_N 27000
-#define NN_MNK_MN 9000
-#define NN_MNK_K 27000
-#define NN_MNK_GE 27000
-#define NT_MNK_M 27000
-#define NT_MNK_N 27000
-#define NT_MNK_MN 9000
-#define NT_MNK_K 27000
-#define NT_MNK_GE 27000
-#define TN_MNK_M 27000
-#define TN_MNK_N 27000
-#define TN_MNK_MN 9000
-#define TN_MNK_K 27000
-#define TN_MNK_GE 27000
-#define TT_MNK_M 27000
-#define TT_MNK_N 27000
-#define TT_MNK_MN 9000
-#define TT_MNK_K 27000
-#define TT_MNK_GE 27000
+#define ATL_3NB 156
+#define NN_MNK_M 146068
+#define NN_MNK_N 146068
+#define NN_MNK_MN 27040
+#define NN_MNK_K 146068
+#define NN_MNK_GE 148877
+#define NT_MNK_M 146068
+#define NT_MNK_N 11700
+#define NT_MNK_MN 27040
+#define NT_MNK_K 29952
+#define NT_MNK_GE 54872
+#define TN_MNK_M 146068
+#define TN_MNK_N 146068
+#define TN_MNK_MN 27040
+#define TN_MNK_K 146068
+#define TN_MNK_GE 148877
+#define TT_MNK_M 146068
+#define TT_MNK_N 146068
+#define TT_MNK_MN 27040
+#define TT_MNK_K 75088
+#define TT_MNK_GE 148877
 
 #endif
--- AtlasBase/Clint/misc/ARCHDEF/Corei264AVX/gemm/gcc/zXover.h
+++ AtlasBase/Clint/misc/ARCHDEF/Corei264AVX/gemm/gcc/zXover.h
@@ -1,27 +1,27 @@
 #ifndef ZXOVER_H
 #define ZXOVER_H
 
-#define ATL_3NB 144
-#define NN_MNK_M 69312
-#define NN_MNK_N 69312
-#define NN_MNK_MN 23040
-#define NN_MNK_K 69312
+#define ATL_3NB 132
+#define NN_MNK_M 63536
+#define NN_MNK_N 4400
+#define NN_MNK_MN 19360
+#define NN_MNK_K 39600
 #define NN_MNK_GE 54872
-#define NT_MNK_M 69312
-#define NT_MNK_N 69312
-#define NT_MNK_MN 23040
-#define NT_MNK_K 69312
-#define NT_MNK_GE 54872
-#define TN_MNK_M 69312
-#define TN_MNK_N 10800
-#define TN_MNK_MN 23040
-#define TN_MNK_K 27648
-#define TN_MNK_GE 27000
-#define TT_MNK_M 69312
-#define TT_MNK_N 43200
-#define TT_MNK_MN 23040
-#define TT_MNK_K 27648
-#define TT_MNK_GE 54872
-#define C2R_K 246
+#define NT_MNK_M 4400
+#define NT_MNK_N 9900
+#define NT_MNK_MN 19360
+#define NT_MNK_K 9900
+#define NT_MNK_GE 13824
+#define TN_MNK_M 4400
+#define TN_MNK_N 4400
+#define TN_MNK_MN 19360
+#define TN_MNK_K 25344
+#define TN_MNK_GE 54872
+#define TT_MNK_M 4400
+#define TT_MNK_N 4400
+#define TT_MNK_MN 19360
+#define TT_MNK_K 9900
+#define TT_MNK_GE 13824
+#define C2R_K 255
 
 #endif
