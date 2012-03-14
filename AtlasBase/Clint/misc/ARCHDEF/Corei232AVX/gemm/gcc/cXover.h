6f657537719d19d5fb4123d946282e945cb9b390
R. Clint Whaley <whaley@cs.utsa.edu>
2012-03-14 18:16:34 -0500

    sSSE alignment ALIGN2X fix & Corei232AVX archdefs

--- AtlasBase/Clint/misc/ARCHDEF/Corei232AVX/gemm/gcc/cXover.h
+++ AtlasBase/Clint/misc/ARCHDEF/Corei232AVX/gemm/gcc/cXover.h
@@ -1,27 +1,27 @@
 #ifndef CXOVER_H
 #define CXOVER_H
 
-#define ATL_3NB 216
-#define NN_MNK_M 7200
-#define NN_MNK_N 7200
-#define NN_MNK_MN 51840
-#define NN_MNK_K 7200
-#define NN_MNK_GE 3375
-#define NT_MNK_M 7200
-#define NT_MNK_N 7200
-#define NT_MNK_MN 51840
-#define NT_MNK_K 16200
-#define NT_MNK_GE 3375
-#define TN_MNK_M 7200
-#define TN_MNK_N 16200
-#define TN_MNK_MN 51840
-#define TN_MNK_K 7200
-#define TN_MNK_GE 3375
-#define TT_MNK_M 7200
-#define TT_MNK_N 7200
-#define TT_MNK_MN 51840
-#define TT_MNK_K 16200
+#define ATL_3NB 228
+#define NN_MNK_M 7600
+#define NN_MNK_N 7600
+#define NN_MNK_MN 57760
+#define NN_MNK_K 109744
+#define NN_MNK_GE 54872
+#define NT_MNK_M 7600
+#define NT_MNK_N 7600
+#define NT_MNK_MN 57760
+#define NT_MNK_K 109744
+#define NT_MNK_GE 27000
+#define TN_MNK_M 7600
+#define TN_MNK_N 7600
+#define TN_MNK_MN 57760
+#define TN_MNK_K 43776
+#define TN_MNK_GE 13824
+#define TT_MNK_M 7600
+#define TT_MNK_N 7600
+#define TT_MNK_MN 57760
+#define TT_MNK_K 17100
 #define TT_MNK_GE 3375
-#define C2R_K 385
+#define C2R_K 639
 
 #endif
--- AtlasBase/Clint/misc/ARCHDEF/Corei232AVX/gemm/gcc/dXover.h
+++ AtlasBase/Clint/misc/ARCHDEF/Corei232AVX/gemm/gcc/dXover.h
@@ -2,25 +2,25 @@
 #define DXOVER_H
 
 #define ATL_3NB 162
-#define NN_MNK_M 151686
-#define NN_MNK_N 31104
+#define NN_MNK_M 5400
+#define NN_MNK_N 5400
 #define NN_MNK_MN 29160
 #define NN_MNK_K 151686
-#define NN_MNK_GE 27000
-#define NT_MNK_M 119286
-#define NT_MNK_N 12150
+#define NN_MNK_GE 148877
+#define NT_MNK_M 151686
+#define NT_MNK_N 5400
 #define NT_MNK_MN 29160
-#define NT_MNK_K 48600
-#define NT_MNK_GE 103823
+#define NT_MNK_K 77976
+#define NT_MNK_GE 148877
 #define TN_MNK_M 5400
-#define TN_MNK_N 12150
+#define TN_MNK_N 151686
 #define TN_MNK_MN 29160
-#define TN_MNK_K 12150
-#define TN_MNK_GE 13824
+#define TN_MNK_K 151686
+#define TN_MNK_GE 148877
 #define TT_MNK_M 5400
-#define TT_MNK_N 12150
+#define TT_MNK_N 5400
 #define TT_MNK_MN 29160
-#define TT_MNK_K 12150
+#define TT_MNK_K 31104
 #define TT_MNK_GE 13824
 
 #endif
