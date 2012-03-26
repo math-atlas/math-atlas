d415dc5b9d53e5e1ed6ce999e951d2a5da833c68
R. Clint Whaley <whaley@cs.utsa.edu>
2012-03-26 09:51:02 -0500

    filled in dozer defaults

---
+++ AtlasBase/Clint/misc/ARCHDEF/AMDDOZER64AVXFMA4/gemm/gcc/cXover.h
@@ -0,0 +1,27 @@
+#ifndef CXOVER_H
+#define CXOVER_H
+
+#define ATL_3NB 216
+#define NN_MNK_M 352800
+#define NN_MNK_N 41472
+#define NN_MNK_MN 51840
+#define NN_MNK_K 352800
+#define NN_MNK_GE 27000
+#define NT_MNK_M 352800
+#define NT_MNK_N 352800
+#define NT_MNK_MN 51840
+#define NT_MNK_K 352800
+#define NT_MNK_GE 343000
+#define TN_MNK_M 41472
+#define TN_MNK_N 41472
+#define TN_MNK_MN 51840
+#define TN_MNK_K 64800
+#define TN_MNK_GE 13824
+#define TT_MNK_M 41472
+#define TT_MNK_N 159048
+#define TT_MNK_MN 51840
+#define TT_MNK_K 267912
+#define TT_MNK_GE 27000
+#define C2R_K 9223372036854775807
+
+#endif
