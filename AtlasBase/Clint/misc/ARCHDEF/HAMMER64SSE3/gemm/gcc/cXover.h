2bec9a3745a95efae7546cbe1a704a9c2193a8b1
R. Clint Whaley <whaley@cs.utsa.edu>
2012-03-28 17:28:48 -0500

    fixed NEON gcc archdefs, added new HAMMER64SSE3

---
+++ AtlasBase/Clint/misc/ARCHDEF/HAMMER64SSE3/gemm/gcc/cXover.h
@@ -0,0 +1,27 @@
+#ifndef CXOVER_H
+#define CXOVER_H
+
+#define ATL_3NB 252
+#define NN_MNK_M 121296
+#define NN_MNK_N 121296
+#define NN_MNK_MN 268128
+#define NN_MNK_K 75600
+#define NN_MNK_GE 103823
+#define NT_MNK_M 48384
+#define NT_MNK_N 8400
+#define NT_MNK_MN 70560
+#define NT_MNK_K 18900
+#define NT_MNK_GE 13824
+#define TN_MNK_M 48384
+#define TN_MNK_N 75600
+#define TN_MNK_MN 169344
+#define TN_MNK_K 18900
+#define TN_MNK_GE 27000
+#define TT_MNK_M 75600
+#define TT_MNK_N 8400
+#define TT_MNK_MN 70560
+#define TT_MNK_K 75600
+#define TT_MNK_GE 103823
+#define C2R_K 648
+
+#endif
---
+++ AtlasBase/Clint/misc/ARCHDEF/HAMMER64SSE3/gemm/gcc/sXover.h
@@ -0,0 +1,26 @@
+#ifndef SXOVER_H
+#define SXOVER_H
+
+#define ATL_3NB 252
+#define NN_MNK_M 8400
+#define NN_MNK_N 8400
+#define NN_MNK_MN 70560
+#define NN_MNK_K 592704
+#define NN_MNK_GE 226981
+#define NT_MNK_M 8400
+#define NT_MNK_N 8400
+#define NT_MNK_MN 70560
+#define NT_MNK_K 121296
+#define NT_MNK_GE 54872
+#define TN_MNK_M 312564
+#define TN_MNK_N 8400
+#define TN_MNK_MN 70560
+#define TN_MNK_K 312564
+#define TN_MNK_GE 226981
+#define TT_MNK_M 8400
+#define TT_MNK_N 8400
+#define TT_MNK_MN 70560
+#define TT_MNK_K 185556
+#define TT_MNK_GE 226981
+
+#endif
