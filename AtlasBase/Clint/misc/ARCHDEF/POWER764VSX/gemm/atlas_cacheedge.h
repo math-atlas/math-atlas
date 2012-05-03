22247c95b9dc2821ad7f283a1245d4119d17cefe
R. Clint Whaley <whaley@cs.utsa.edu>
2012-05-03 17:29:32 -0500

    new archdefs for Corei264SSE3 & POWER264VSX

--- AtlasBase/Clint/misc/ARCHDEF/POWER764VSX/gemm/atlas_cacheedge.h
+++ AtlasBase/Clint/misc/ARCHDEF/POWER764VSX/gemm/atlas_cacheedge.h
@@ -1,4 +1,4 @@
 #ifndef ATLAS_CACHEEDGE_H
    #define ATLAS_CACHEEDGE_H
-   #define CacheEdge 1048576
+   #define CacheEdge 786432
 #endif
--- AtlasBase/Clint/misc/ARCHDEF/POWER764VSX/gemm/gcc/zXover.h
+++ AtlasBase/Clint/misc/ARCHDEF/POWER764VSX/gemm/gcc/zXover.h
@@ -1,27 +1,27 @@
 #ifndef ZXOVER_H
 #define ZXOVER_H
 
-#define ATL_3NB 240
-#define NN_MNK_M 564480
-#define NN_MNK_N 564480
-#define NN_MNK_MN 64000
-#define NN_MNK_K 564480
-#define NN_MNK_GE 592704
-#define NT_MNK_M 297680
-#define NT_MNK_N 297680
-#define NT_MNK_MN 64000
-#define NT_MNK_K 564480
-#define NT_MNK_GE 592704
-#define TN_MNK_M 564480
-#define TN_MNK_N 564480
-#define TN_MNK_MN 64000
-#define TN_MNK_K 564480
-#define TN_MNK_GE 592704
-#define TT_MNK_M 564480
-#define TT_MNK_N 392000
-#define TT_MNK_MN 64000
-#define TT_MNK_K 564480
-#define TT_MNK_GE 592704
+#define ATL_3NB 192
+#define NN_MNK_M 1081600
+#define NN_MNK_N 1081600
+#define NN_MNK_MN 61440
+#define NN_MNK_K 1081600
+#define NN_MNK_GE 2197000
+#define NT_MNK_M 6400
+#define NT_MNK_N 92416
+#define NT_MNK_MN 40960
+#define NT_MNK_K 1081600
+#define NT_MNK_GE 2197000
+#define TN_MNK_M 6400
+#define TN_MNK_N 1081600
+#define TN_MNK_MN 40960
+#define TN_MNK_K 1081600
+#define TN_MNK_GE 2197000
+#define TT_MNK_M 6400
+#define TT_MNK_N 1081600
+#define TT_MNK_MN 40960
+#define TT_MNK_K 1081600
+#define TT_MNK_GE 2197000
 #define C2R_K 76
 
 #endif
