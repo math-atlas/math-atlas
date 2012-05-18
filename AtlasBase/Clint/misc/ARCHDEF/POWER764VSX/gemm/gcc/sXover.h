e3b32fe8da88efd55b95676e38d7c9482d62ecaf
R. Clint Whaley <whaley@cs.utsa.edu>
2012-05-17 21:22:31 -0500

    fixed power7 bug in configure for gcc selection

--- AtlasBase/Clint/misc/ARCHDEF/POWER764VSX/gemm/gcc/sXover.h
+++ AtlasBase/Clint/misc/ARCHDEF/POWER764VSX/gemm/gcc/sXover.h
@@ -2,25 +2,25 @@
 #define SXOVER_H
 
 #define ATL_3NB 240
-#define NN_MNK_M 115520
-#define NN_MNK_N 564480
-#define NN_MNK_MN 243200
+#define NN_MNK_M 8000
+#define NN_MNK_N 297680
+#define NN_MNK_MN 64000
 #define NN_MNK_K 8000
-#define NN_MNK_GE 592704
-#define NT_MNK_M 115520
-#define NT_MNK_N 8000
+#define NN_MNK_GE 1000
+#define NT_MNK_M 8000
+#define NT_MNK_N 297680
 #define NT_MNK_MN 64000
-#define NT_MNK_K 564480
-#define NT_MNK_GE 592704
-#define TN_MNK_M 564480
-#define TN_MNK_N 564480
-#define TN_MNK_MN 243200
+#define NT_MNK_K 8000
+#define NT_MNK_GE 1000
+#define TN_MNK_M 8000
+#define TN_MNK_N 297680
+#define TN_MNK_MN 64000
 #define TN_MNK_K 8000
-#define TN_MNK_GE 592704
-#define TT_MNK_M 224720
-#define TT_MNK_N 8000
-#define TT_MNK_MN 192000
+#define TN_MNK_GE 1000
+#define TT_MNK_M 8000
+#define TT_MNK_N 297680
+#define TT_MNK_MN 64000
 #define TT_MNK_K 8000
-#define TT_MNK_GE 592704
+#define TT_MNK_GE 1000
 
 #endif
--- AtlasBase/Clint/misc/ARCHDEF/POWER764VSX/lapack/gcc/atlas_sGetNB_geqrf.h
+++ AtlasBase/Clint/misc/ARCHDEF/POWER764VSX/lapack/gcc/atlas_sGetNB_geqrf.h
@@ -4,13 +4,13 @@
  * NB selection for GEQRF: Side='RIGHT', Uplo='UPPER'
  * M : 25,160,240,320,400,880,1760,3600
  * N : 25,160,240,320,400,880,1760,3600
- * NB : 5,12,20,20,80,80,80,80
+ * NB : 4,20,16,16,80,80,80,80
  */
 #define ATL_sGetNB_geqrf(n_, nb_) \
 { \
-   if ((n_) < 92) (nb_) = 5; \
-   else if ((n_) < 200) (nb_) = 12; \
-   else if ((n_) < 360) (nb_) = 20; \
+   if ((n_) < 92) (nb_) = 4; \
+   else if ((n_) < 200) (nb_) = 20; \
+   else if ((n_) < 360) (nb_) = 16; \
    else (nb_) = 80; \
 }
 
