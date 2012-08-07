5648863eb7ca5bca04e249fd537f727cbdeba8cf
msujon <msujon>
2012-08-07 16:54:17 +0000

    add prototypes

--- include/fko_optreg.h
+++ include/fko_optreg.h
@@ -1,14 +1,12 @@
 #ifndef FKO_OPTREG_H
 #define FKO_OPTREG_H
 
-char *Int2Reg0(int i);
 char *Int2Reg(int i);
+char *Int2Reg0(int i);
 short Reg2Int(char *regname);
 int NumberArchRegs();
 void CalcIG(BLIST *blist);
 int DoScopeRegAsg(BLIST *scope, int thresh, int *tnig);
 int DoCopyProp(BLIST *scope);
 int DoLoopGlobalRegAssignment(LOOPQ *loop);
-
-
 #endif
