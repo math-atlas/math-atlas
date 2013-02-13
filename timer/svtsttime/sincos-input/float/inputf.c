7a11d8f0ef9dc9708ecf753fe59e919561b78902
msujon <msujon>
2013-02-13 21:40:16 +0000

    added to generate inputs of sin and cos

---
+++ timer/svtsttime/sincos-input/float/inputf.c
@@ -0,0 +1,48 @@
+#include "private.h"
+/*
+ * it works for for sin and cos 
+ */
+
+int inputf(float x, float *y1, float *y2)
+{
+   float y[2], z=0.0;
+   int n, ix;
+   
+   n = 0;
+   GET_FLOAT_WORD(ix, x);
+   ix &= 0x7fffffff;
+   
+   if (ix <= 0x3f490fd8)
+   {
+      *y1 = x;
+      *y2 = z;
+   }
+   else if (ix >= 0x7f800000)
+   {
+      *y1 = NA;
+      *y2 = NA;
+   }
+   else
+   {
+      n = __ieee754_rem_pio2f(x,y);
+      switch(n&3)
+      {
+         case 0:
+            *y1 = y[0];
+            *y2 = y[1];
+            break;
+         case 1:
+            *y1 = NA;
+            *y2 = NA;
+            break;
+         case 2:
+            *y1 = y[0];
+            *y2 = y[1];
+            break;
+         default:
+            *y1 = NA;
+            *y2 = NA;
+      }
+   }
+   return (n&3);
+}
