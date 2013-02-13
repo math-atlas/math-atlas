fc0f9bc55e25a0ddc23f94cc44b27e1c3adb91a4
msujon <msujon>
2013-02-13 21:42:25 +0000

    input for sin cos for double precision

---
+++ timer/svtsttime/sincos-input/double/input.c
@@ -0,0 +1,49 @@
+
+#include "private.h"
+
+int inputd(double x, double *y1, double *y2)
+{
+   double y[2], z=0.0;
+   int n, ix;
+   
+   n = 0;
+   GET_HIGH_WORD(ix, x);
+   ix &= 0x7fffffff;
+   
+   if (ix <= 0x3fe921fb)
+   {
+      *y1 = x;
+      *y2 = z;
+   }
+   else if (ix >= 0x7ff00000)
+   {
+      *y1 = NA;
+      *y2 = NA;
+   }
+   else
+   {
+      n = __ieee754_rem_pio2(x,y);
+      switch(n&3)
+      {
+         case 0:
+            *y1 = y[0];
+            *y2 = y[1];
+            break;
+         case 1:
+            *y1 = NA;
+            *y2 = NA;
+            break;
+         case 2:
+            *y1 = y[0];
+            *y2 = y[1];
+            break;
+         default:
+            *y1 = NA;
+            *y2 = NA;
+      }
+   }
+   return n;
+}
+
+
+
