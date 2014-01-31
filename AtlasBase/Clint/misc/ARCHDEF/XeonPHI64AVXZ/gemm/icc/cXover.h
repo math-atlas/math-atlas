df2a25b1b37ba7b853e737fda470a5693230009f
R. Clint Whaley <whaley@cs.utsa.edu>
2014-01-31 11:38:22 -0600

    PHI archdefs where bigMM works, small/ammm fails

---
+++ AtlasBase/Clint/misc/ARCHDEF/XeonPHI64AVXZ/gemm/icc/cXover.h
@@ -0,0 +1,26 @@
+#ifndef CXOVER_H
+#define CXOVER_H
+
+#define ATL_3NB 1
+#define NN_MNK_M 1
+#define NN_MNK_N 1
+#define NN_MNK_MN 1
+#define NN_MNK_K 1
+#define NN_MNK_GE 1
+#define NT_MNK_M 1
+#define NT_MNK_N 1
+#define NT_MNK_MN 1
+#define NT_MNK_K 1
+#define NT_MNK_GE 1
+#define TN_MNK_M 1
+#define TN_MNK_N 1
+#define TN_MNK_MN 1
+#define TN_MNK_K 1
+#define TN_MNK_GE 1
+#define TT_MNK_M 1
+#define TT_MNK_N 1
+#define TT_MNK_MN 1
+#define TT_MNK_K 1
+#define TT_MNK_GE 1
+
+#endif
