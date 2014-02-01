c90b049e63beeaabef9c116c7617f310a2c2f9a5
R. Clint Whaley <whaley@cs.utsa.edu>
2014-02-01 15:11:46 -0600

    single prec PHI kernels

--- AtlasBase/Clint/misc/ARCHDEF/XeonPHI64AVXZ/gemm/icc/zXover.h
+++ AtlasBase/Clint/misc/ARCHDEF/XeonPHI64AVXZ/gemm/icc/zXover.h
@@ -1,26 +1,27 @@
 #ifndef ZXOVER_H
 #define ZXOVER_H
 
-#define ATL_3NB 1
-#define NN_MNK_M 1
-#define NN_MNK_N 1
-#define NN_MNK_MN 1
-#define NN_MNK_K 1
-#define NN_MNK_GE 1
-#define NT_MNK_M 1
-#define NT_MNK_N 1
-#define NT_MNK_MN 1
-#define NT_MNK_K 1
-#define NT_MNK_GE 1
-#define TN_MNK_M 1
-#define TN_MNK_N 1
-#define TN_MNK_MN 1
-#define TN_MNK_K 1
-#define TN_MNK_GE 1
-#define TT_MNK_M 1
-#define TT_MNK_N 1
-#define TT_MNK_MN 1
-#define TT_MNK_K 1
-#define TT_MNK_GE 1
+#define ATL_3NB 120
+#define NN_MNK_M 4000
+#define NN_MNK_N 9000
+#define NN_MNK_MN 16000
+#define NN_MNK_K 4000
+#define NN_MNK_GE 3375
+#define NT_MNK_M 9000
+#define NT_MNK_N 9000
+#define NT_MNK_MN 16000
+#define NT_MNK_K 4000
+#define NT_MNK_GE 3375
+#define TN_MNK_M 4000
+#define TN_MNK_N 4000
+#define TN_MNK_MN 16000
+#define TN_MNK_K 4000
+#define TN_MNK_GE 3375
+#define TT_MNK_M 4000
+#define TT_MNK_N 4000
+#define TT_MNK_MN 16000
+#define TT_MNK_K 4000
+#define TT_MNK_GE 3375
+#define C2R_K 1000
 
 #endif
