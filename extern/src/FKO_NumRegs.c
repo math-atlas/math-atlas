ef5bfbe32f23a05a2fa2042a2c96130a433db646
whaley <whaley>
2015-06-05 15:01:37 +0000

    *** empty log message ***

--- extern/src/FKO_NumRegs.c
+++ extern/src/FKO_NumRegs.c
@@ -1,29 +1,12 @@
-#include "fko_archinfo.h"
+#include "fko_info.h"
 
-int FKO_NumRegsC(fko_archinfo_t *ap, int typ1)
+int FKO_NumRegsC(fko_archinfo_t *ap, int typ)
 {
-   switch(typ1)
-   {
-   case FKO_TINT:
-      return(ap->numregs[FKO_TINT]);
-   case FKO_TFLT:
-      return(ap->numregs[FKO_TFLT]);
-   case FKO_TDBL:
-      return(ap->numregs[FKO_TDBL]);
-   case FKO_TVINT:
-      return(ap->numregs[FKO_TVINT]);
-   case FKO_TVFLT:
-      return(ap->numregs[FKO_TVFLT]);
-   case FKO_TVDBL:
-      return(ap->numregs[FKO_TVDBL]);
-   default:
-      fprintf(stderr, "INVALID TYPE=%d\n", typ1);
-      assert(0);
-
-   }
-   return(0);
+   assert(typ < FKO_NTYPES && typ >= 0);
+   return(ap->numregs[typ]);
 }
-int FKO_NumRegs(int typ1)
+
+int FKO_NumRegs(int typ)
 {
-   return(FKO_NumRegsC(FKO_ARCHINF, typ1));
+   return(FKO_NumRegsC(FKO_ARCHINF, typ));
 }
