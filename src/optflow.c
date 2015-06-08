167eac9da1cdd577dfc31e93650fbb74a1473982
whaley <whaley>
2015-06-08 22:41:15 +0000

    *** empty log message ***

--- src/optflow.c
+++ src/optflow.c
@@ -1,7 +1,6 @@
 /*
  * Copyright (C) 2004, 2015 R. Clint Whaley.
  * Code contributers : R. Clint Whaley, Majedul Sujon.
- * See iFKO/LICENSE.txt for software licence (3-clause BSD).
  */
 #include "fko.h"
 
