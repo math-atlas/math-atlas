b1cbd1d753fd8d37c6e8f64f896f3646b5d33b6c
R. Clint Whaley <whaley@cs.utsa.edu>
2016-06-20 22:24:41 -0500

    Z/pwr8 fixes

--- AtlasBase/Clint/misc/ARCHDEF/IBMz1364VXZ/gemm/atlas_cacheedge.h
+++ AtlasBase/Clint/misc/ARCHDEF/IBMz1364VXZ/gemm/atlas_cacheedge.h
@@ -1,4 +1,8 @@
 #ifndef ATLAS_CACHEEDGE_H
    #define ATLAS_CACHEEDGE_H
-   #define CacheEdge 1048576
+   #define CacheEdge 262144
+   #ifdef DCPLX
+      #undef CacheEdge
+      #define CacheEdge 1048576
+   #endif
 #endif
