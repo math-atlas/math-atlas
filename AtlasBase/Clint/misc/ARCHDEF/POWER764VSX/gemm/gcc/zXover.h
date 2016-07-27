8f467b085a1c111aa3103891ee1775788de3847a
R. Clint Whaley <whaley@cs.utsa.edu>
2016-07-27 11:05:17 -0500

    got rid of -m64

---
+++ AtlasBase/Clint/misc/ARCHDEF/POWER764VSX/gemm/gcc/zXover.h
@@ -0,0 +1,27 @@
+#ifndef ZXOVER_H
+#define ZXOVER_H
+
+#define ATL_3NB 168
+#define NN_MNK_M 5600
+#define NN_MNK_N 5600
+#define NN_MNK_MN 31360
+#define NN_MNK_K 80864
+#define NN_MNK_GE 1225043
+#define NT_MNK_M 5600
+#define NT_MNK_N 5600
+#define NT_MNK_MN 31360
+#define NT_MNK_K 80864
+#define NT_MNK_GE 1225043
+#define TN_MNK_M 5600
+#define TN_MNK_N 5600
+#define TN_MNK_MN 31360
+#define TN_MNK_K 80864
+#define TN_MNK_GE 1225043
+#define TT_MNK_M 5600
+#define TT_MNK_N 5600
+#define TT_MNK_MN 31360
+#define TT_MNK_K 80864
+#define TT_MNK_GE 1225043
+#define C2R_K 61
+
+#endif
