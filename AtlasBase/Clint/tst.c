8464139604a253e740509846e3a3961773fb2db5
R. Clint Whaley <whaley@cs.utsa.edu>
2017-01-02 13:40:33 -0600

    almost ready for ammkgen

--- AtlasBase/Clint/tst.c
+++ AtlasBase/Clint/tst.c
@@ -1,8 +1,14 @@
-#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "/home/whaley/TEST/ATLAS3.11.38/obj64_8/include/atlas_dgeamm_kern.h"
 
-void bob()
+int main(void)
 {
-   pthread_cond_t condvar;
-   pthread_cond_t *cond;
-   cond = &condvar;
+   int i;
+   for (i=0; i < ATL_AMM_NCASES; i++)
+   {
+      printf("kerns=%p, %p, %p\n", 
+             ATL_AMM_KERN_b0[i], ATL_AMM_KERN_b1[i], ATL_AMM_KERN_bn[i]);
+   }
+   return(0);
 }
